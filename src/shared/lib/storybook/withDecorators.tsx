7dac5de0d1bc6d42e9b971a9a628471e217f54c5
ZeiZel <valera2003lvov@yandex.ru>
2023-11-21 18:38:42 +0300

    main: loki test ready

--- src/shared/lib/storybook/withDecorators.tsx
+++ src/shared/lib/storybook/withDecorators.tsx
@@ -3,16 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import React, { Suspense } from 'react';
 import { Theme, ThemeProvider } from '@/app/providers/ThemeProvider';
 
-export const withDecorators =
-	(theme: Theme = Theme.LIGHT) =>
-	(StoryComponent: StoryFn) => (
-		<BrowserRouter>
-			<Suspense>
-				<ThemeProvider>
-					<div className={`app ${theme}`}>
-						<StoryComponent />
-					</div>
-				</ThemeProvider>
-			</Suspense>
-		</BrowserRouter>
-	);
+export const withDecorators = (StoryComponent: StoryFn) => (
+	<BrowserRouter>
+		<Suspense>
+			<ThemeProvider>
+				<div className={`app ${Theme.LIGHT}`}>
+					<StoryComponent />
+				</div>
+			</ThemeProvider>
+		</Suspense>
+	</BrowserRouter>
+);
--- src/shared/ui/Button/ui/Button.stories.tsx
+++ src/shared/ui/Button/ui/Button.stories.tsx
@@ -2,12 +2,11 @@ import { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
 import { EButtonType } from '../model';
 import { withDecorators } from '@/shared/lib';
-import { Theme } from '@/app/providers/ThemeProvider';
 
 const meta: Meta<typeof Button> = {
-	title: 'Components/UI/Button',
+	title: 'Shared/Button',
 	component: Button,
-	decorators: [withDecorators(Theme.LIGHT)],
+	decorators: [withDecorators],
 };
 export default meta;
 
